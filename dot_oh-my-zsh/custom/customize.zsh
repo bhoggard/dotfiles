dfcf9a53db887f90cfbbce85312b3ec5bda6d408
Barry Hoggard <barry@hoggard.org>
2025-10-15 21:19:06 -0400

    Update .oh-my-zsh/custom/customize.zsh

--- dot_oh-my-zsh/custom/customize.zsh
+++ dot_oh-my-zsh/custom/customize.zsh
@@ -1,5 +1,9 @@
 export PATH="$HOME/bin:$PATH"
-export PATH=/Applications/Postgres.app/Contents/Versions/16/bin:$PATH
+export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
+
+export LDFLAGS="-L/opt/homebrew/opt/postgresql@17/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/postgresql@17/include"
+
 export EDITOR=nvim
 export HOMEBREW_NO_ENV_HINTS=true
 
@@ -12,4 +16,5 @@ alias cm=chezmoi
 eval "$(/opt/homebrew/bin/brew shellenv)"
 eval "$(ssh-agent -s)"
 eval "$(/opt/homebrew/bin/mise activate zsh)"
+
 
